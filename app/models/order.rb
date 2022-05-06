d7a74a3b657888e4f2faa2cd4ed632f28a49d7d3
mberrueta <matiasberrueta@gmail.com>
2022-05-06 20:20:41 -0300

    Added kafka  gem

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require 'kafka_rails_integration'
+
 class Order < ApplicationRecord
+  include KafkaRailsIntegration::Concerns::Model::Eventeable
   include Itemable
 
   validates :organization_id, :customer_id, :date, :deliver_date, presence: true
--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,7 @@ module OrdersApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use(KafkaRailsIntegration::Middlewares::DeliverMessages)
   end
 end
