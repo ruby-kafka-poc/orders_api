764f409edd7cf124fb81736005c2835d2f539438
mberrueta <matiasberrueta@gmail.com>
2022-10-06 16:18:40 -0300

    merge

--- README.md
+++ README.md
@@ -1,24 +1,60 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Simple API to create orders and invoices
+It act as a producer to confluent Kafka
 
-Things you may want to cover:
+## Run
 
-* Ruby version
+You will need postgres running (you have the docker-compose)
 
-* System dependencies
+```shell
+git clone git@github.com:ruby-kafka-poc/orders_api.git
+cd orders_api
 
-* Configuration
+open https://confluent.cloud
+# Create a free account (no credit card needed free for 1 month), create a cluster,
+# create a global API key
+# go to cluster, cluster overview, cluster settings and get the bootstrap server
 
-* Database creation
+# WARNING DO NOT COMMIT THIS THINGS ANYWHERE!!!
+echo "
+BOOTSTRAP_SERVERS=AAAAAAAAAA:9092
+SECURITY_PROTOCOL=sasl_ssl
+SASL_MECHANISM=PLAIN
+SASL_USERNAME=BBBBBB
+SASL_PASSWORD=CCCCCCC
+" >> .priv_env
+export $(cat ./.private_env | sed 's/#.*//g' | xargs )
 
-* Database initialization
 
-* How to run the test suite
+bundle install
+bundle update
+bundle exec rake db:create db:migrate db:seed
+```
 
-* Services (job queues, cache servers, search engines, etc.)
+## Routes
 
-* Deployment instructions
-
-* ...
+```shell
+GET    /invoices/:invoice_id/items
+POST   /invoices/:invoice_id/items
+PATCH  /invoices/:invoice_id/items/:id
+PUT    /invoices/:invoice_id/items/:id
+DELETE /invoices/:invoice_id/items/:id
+GET    /invoices
+POST   /invoices
+GET    /invoices/:id
+PATCH  /invoices/:id
+PUT    /invoices/:id
+DELETE /invoices/:id
+GET    /orders/:order_id/items
+POST   /orders/:order_id/items
+PATCH  /orders/:order_id/items/:id
+PUT    /orders/:order_id/items/:id
+DELETE /orders/:order_id/items/:id
+GET    /orders
+POST   /orders
+GET    /orders/:id
+PATCH  /orders/:id
+PUT    /orders/:id
+DELETE /orders/:id
+```
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,45 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_502_140_137) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_140137) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'invoices', force: :cascade do |t|
-    t.string 'po'
-    t.integer 'organization_id', null: false
-    t.integer 'customer_id', null: false
-    t.datetime 'date', null: false
-    t.datetime 'deliver_date', null: false
-    t.string 'state'
-    t.bigint 'order_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['order_id'], name: 'index_invoices_on_order_id'
+  create_table "invoices", force: :cascade do |t|
+    t.string "po"
+    t.integer "organization_id", null: false
+    t.integer "customer_id", null: false
+    t.datetime "date", null: false
+    t.datetime "deliver_date", null: false
+    t.string "state"
+    t.bigint "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_invoices_on_order_id"
   end
 
-  create_table 'items', force: :cascade do |t|
-    t.string 'itemable_type', null: false
-    t.bigint 'itemable_id', null: false
-    t.integer 'quantity', null: false
-    t.string 'description', null: false
-    t.string 'code', null: false
-    t.decimal 'amount', precision: 11, scale: 2, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[itemable_type itemable_id], name: 'index_items_on_itemable'
+  create_table "items", force: :cascade do |t|
+    t.string "itemable_type", null: false
+    t.bigint "itemable_id", null: false
+    t.integer "quantity", null: false
+    t.string "description", null: false
+    t.string "code", null: false
+    t.decimal "amount", precision: 11, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["itemable_type", "itemable_id"], name: "index_items_on_itemable"
   end
 
-  create_table 'orders', force: :cascade do |t|
-    t.string 'po'
-    t.integer 'organization_id', null: false
-    t.integer 'customer_id', null: false
-    t.datetime 'date', null: false
-    t.datetime 'deliver_date', null: false
-    t.string 'state'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "orders", force: :cascade do |t|
+    t.string "po"
+    t.integer "organization_id", null: false
+    t.integer "customer_id", null: false
+    t.datetime "date", null: false
+    t.datetime "deliver_date", null: false
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'invoices', 'orders'
+  add_foreign_key "invoices", "orders"
 end
